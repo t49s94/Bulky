e1e3bd25a18cf50e34cc504a279e686013c0d561
Sergio Santillana <t49s94@gmail.com>
2024-09-28 14:23:41 -0400

    Add project files.

---
+++ BulkyWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        //base(options) => pass options to the base class(DbContext)
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+    }
+}
